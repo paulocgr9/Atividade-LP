529791b35a25e05f37eb2105884e5deb745efb8f
GuilhermeCastilho02 <126675994+GuilhermeCastilho02@users.noreply.github.com>
2023-08-16 09:08:20 -0300

    Add files via upload

--- questao_2.py
+++ questao_2.py
@@ -6,7 +6,4 @@ def questao_2(size=8):
     a1D_3 = questao_1()
     a2D_3_redimensionado = np.reshape(a1D_3, (2, size//2))
     a2D_3_float = a2D_3_redimensionado / 1
-    a2D_3_float = float(a2D_3_redimensionado)
     return a2D_3_float
-
-print(questao_2())
--- questao_3.py
+++ questao_3.py
@@ -1,10 +1,10 @@
 import numpy as np
 import numpy.random as npr
-
+from questao_2 import questao_2
 
 
 def questao_3(matriz, size=8):
     a2D_4 = npr.randint(100, size = size)
-    a2D_4 = a2D_4.reshape(2, size/2)
+    a2D_4 = a2D_4.reshape(2, size//2)
     a2D_4 = a2D_4 * matriz
     return a2D_4
