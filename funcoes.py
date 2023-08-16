ee39d05561e0a2cb96f31cff57ba49c5e00faf25
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-08-16 08:18:51 -0300

    Adicionado questão 1

--- funcoes.py
+++
@@ -1,2 +0,0 @@
-import numpy
-import numpy.random as npr
---
+++ questao_1.py
@@ -0,0 +1,9 @@
+import numpy
+import numpy.random as npr
+
+def questao_1():
+    a1D_1 = npr.randint(100, size=8)
+    a2D_2 = npr.randint(100, size=8)
+    return a2D_2
+
+print(questao_1())
